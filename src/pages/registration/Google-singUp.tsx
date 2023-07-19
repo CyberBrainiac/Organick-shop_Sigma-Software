97dba51cbbbfd32a89905fdf317fbc7060ead716
Arseniy <arsenij.arxipov.1998@gmail.com>
2023-07-19 15:50:21 +0300

    create declaration file to script.js+google button

---
+++ src/pages/registration/Google-singUp.tsx
@@ -0,0 +1,33 @@
+import userSignupCredentialHandler from "../../utils/auth/sign-up.js"
+
+const userCredential = {
+  username: 'exampleuser',
+  password: 'examplepassword',
+  email: 'user@example.com',
+};
+
+userSignupCredentialHandler(userCredential);
+
+export default function GoogleSignUp() {
+  return (
+    <div className='googleSignUp'>
+      <div id="g_id_onload"
+          data-client_id="864090627228-2kqbbpejhd9bpe118jbfmh2id3hrrf2l.apps.googleusercontent.com"
+          data-context="signup"
+          data-ux_mode="popup"
+          data-callback="userSignupCredentialHandler"
+          data-auto_prompt="false">
+      </div>
+
+      <div className="g_id_signin"
+          data-type="standard"
+          data-shape="rectangular"
+          data-theme="filled_blue"
+          data-text="signup_with"
+          data-size="large"
+          data-locale="en"
+          data-logo_alignment="left">
+      </div>
+    </div>
+  );
+}
---
+++ src/utils/auth/sign-up.js
@@ -0,0 +1,4 @@
+
+export default function userSignupCredentialHandler(userCredential) {
+  console.log(userCredential);
+}
